6e55473ebb1e851794d56db93d4915c28bd4413e
fadhilwj <m.fadhillah.w@gmail.com>
2024-11-28 02:38:51 +0700

    opencv1.1

--- Muhammad Fadhillah Wijaya_shapeColorDetection.py
+++ Muhammad Fadhillah Wijaya_shapeColorDetection.py
@@ -1,101 +1,77 @@
-import cv2
+# Python code for Multiple Color Detection 
+
+
+import numpy as np 
+import cv2 
 from cv2 import approxPolyDP
-import numpy as np
 
+# Capturing video through webcam 
+webcam = cv2.VideoCapture(0) 
 
-def getColor(hsv,frame,warna):
-    #lowG = np.load('/home/fadhil/Documents/Materi_OpenCV/green_low.npy')  # Memuat batas bawah warna hijau dari file
-    #highG = np.load('/home/fadhil/Documents/Materi_OpenCV/green_high.npy')  # Memuat batas atas warna hijau dari file
-    if warna == "Hijau":
-        low = np.array([8,168,0])
-        high = np.array([149,255,255])
-    elif warna == "Biru":
-        pass
-    elif warna == "Merah":
-        pass
-    elif warna == "Kuning":
-        pass
-    else:
-        print("Warna tidak terseida.")
-        return
+# Start a while loop 
+while(1): 
+    
+    # Reading the video from the 
+    # webcam in image frames 
+    _, imageFrame = webcam.read() 
 
-    thresh = cv2.inRange(hsv, low, high)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-    return thresh
+    # Convert the imageFrame in 
+    # BGR(RGB color space) to 
+    # HSV(hue-saturation-value) 
+    # color space 
+    hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV) 
 
+    # Set range for red color and 
+    # define mask 
+    red_lower = np.array([136, 87, 111], np.uint8) 
+    red_upper = np.array([180, 255, 255], np.uint8) 
+    red_mask = cv2.inRange(hsvFrame, red_lower, red_upper) 
 
-def getContour(thresh, frameContour):
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        if area > 500:
-            cv2.drawContours(frameContour, cnt, -1, (0,0,255), 2)
-            peri = cv2.arcLength(cnt, True)
-            approx = approxPolyDP(cnt, 0.02*peri, True)
-            korner = len(approx)
-            print(korner)
-            
-            x,y,w,h = cv2.boundingRect(approx)
+    # Set range for green color and 
+    # define mask 
+    green_lower = np.array([25, 52, 72], np.uint8) 
+    green_upper = np.array([102, 255, 255], np.uint8) 
+    green_mask = cv2.inRange(hsvFrame, green_lower, green_upper) 
 
-            if korner == 3: benda = "Segitiga"
-            elif korner == 4:
-                rasio = w/float(h)
-                if 0.9 < rasio < 1.1: benda = "Persegi"
-                else: benda = "Persegi Panjang"
-            else: benda = "Lainnya"
+    # Set range for blue color and 
+    # define mask 
+    blue_lower = np.array([94, 80, 2], np.uint8) 
+    blue_upper = np.array([120, 255, 255], np.uint8) 
+    blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper) 
+    
+    # Morphological Transform, Dilation 
+    # for each color and bitwise_and operator 
+    # between imageFrame and mask determines 
+    # to detect only that particular color 
+    kernel = np.ones((5, 5), "uint8") 
+    
+    # For red color
+    red_mask = cv2.dilate(red_mask, kernel) 
+    res_red = cv2.bitwise_and(imageFrame, imageFrame, 
+                            mask = red_mask)
+    
+    # For green color 
+    green_mask = cv2.dilate(green_mask, kernel) 
+    res_green = cv2.bitwise_and(imageFrame, imageFrame, 
+                                mask = green_mask) 
+    # For blue color
+    blue_mask = cv2.dilate(blue_mask, kernel) 
+    res_blue = cv2.bitwise_and(imageFrame, imageFrame, 
+                            mask = blue_mask) 
 
-            cv2.rectangle(frameContour, (x,y), (x+w, y+h), (255,0,0), 2)
-            cv2.putText(frameContour, benda, (x+(w//2)-10, y+(h//2)-10), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,0,0), 2)
-
-
-def main(video):
-    while True:
-        ret, frame = video.read()
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # Hijau
-        thresh = getColor(hsv,frame, "Hijau")
-        #contour, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        frameContour = frame.copy()
-        frameBlur = cv2.GaussianBlur(thresh, (7,7), 1)
-        frameCanny = cv2.Canny(frameBlur, 50, 70)
-
-        getContour(frameCanny, frameContour)
-        
-        # Biru
-
-        # Merah
-
-        # Kuning
-
-        """
-        if contour :
-            largest_contour = max(contour,key=cv2.contourArea)
-
-            x, y, w, h = cv2.boundingRect(largest_contour)
-
-            cv2.rectangle(frame, (x, y), (w+x, h+y), (255, 0, 0), 2)
-        """
-        #shape
-        
-
-        cv2.imshow("Asli", frame)
-        cv2.imshow("Canny", frameCanny)
-        #cv2.imshow("Blur", frameBlur)
-        cv2.imshow("Hasil", frameContour)
-
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-
-    video.release()
-    cv2.destroyAllWindows()
-
-if __name__ == '__main__':
-    cam = cv2.VideoCapture(-1)
-    main(cam)
-
-
-"""         
+    # Creating contour to track red color
+    contours, hierarchy = cv2.findContours(red_mask, 
+                                        cv2.RETR_TREE, 
+                                        cv2.CHAIN_APPROX_SIMPLE) 
+    
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 500): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            imageFrame = cv2.rectangle(imageFrame, (x, y), 
+                                    (x + w, y + h), 
+                                    (0, 0, 255), 2) 
+ 
             cv2.drawContours(imageFrame, contour, -1, (0,0,255), 2)
             peri = cv2.arcLength(contour, True)
             approx = approxPolyDP(contour, 0.02*peri, True)
@@ -116,6 +92,81 @@ if __name__ == '__main__':
             #elif korner <= 10000: benda = "Lingkaran"
             else: benda = "Lainnya"
 
-            cv2.rectangle(imageFrame, (x,y), (x+w, y+h), (255,0,0), 2)
-            cv2.putText(imageFrame, benda, (x+(w//2)-10, y+(h//2)-10), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,255,0), 2)
-            """
+            cv2.putText(imageFrame, benda+" MERAH", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,0,255), 2)  
+
+    # Creating contour to track green color 
+    contours, hierarchy = cv2.findContours(green_mask, 
+                                        cv2.RETR_TREE, 
+                                        cv2.CHAIN_APPROX_SIMPLE) 
+    
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 500): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            imageFrame = cv2.rectangle(imageFrame, (x, y), 
+                                    (x + w, y + h), 
+                                    (0, 255, 0), 2) 
+            cv2.drawContours(imageFrame, contour, -1, (0,0,255), 2)
+            peri = cv2.arcLength(contour, True)
+            approx = approxPolyDP(contour, 0.02*peri, True)
+            korner = len(approx)
+            print(korner)
+            
+            x,y,w,h = cv2.boundingRect(approx)
+
+            if korner == 3: benda = "Segitiga"
+            elif korner == 4:
+                rasio = w/float(h)
+                if 0.9 < rasio < 1.1: benda = "Persegi"
+                else: benda = "Persegi Panjang"
+            #elif korner == 5: benda = "Segi lima"
+            #elif korner == 6: benda = "Segi enam"
+            #elif korner == 7: benda = "Segi tujuh"
+            #elif korner == 8: benda = "Segi delapan"
+            #elif korner <= 10000: benda = "Lingkaran"
+            else: benda = "Lainnya"
+
+            cv2.putText(imageFrame, benda+" HIJAU", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,255,0), 2)  
+
+    # Creating contour to track blue color
+    contours, hierarchy = cv2.findContours(blue_mask, 
+                                        cv2.RETR_TREE, 
+                                        cv2.CHAIN_APPROX_SIMPLE) 
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 500): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            imageFrame = cv2.rectangle(imageFrame, (x, y), 
+                                    (x + w, y + h), 
+                                    (255, 0, 0), 2)
+            
+            cv2.drawContours(imageFrame, contour, -1, (0,0,255), 2)
+            peri = cv2.arcLength(contour, True)
+            approx = approxPolyDP(contour, 0.02*peri, True)
+            korner = len(approx)
+            print(korner)
+            
+            x,y,w,h = cv2.boundingRect(approx)
+
+            if korner == 3: benda = "Segitiga"
+            elif korner == 4:
+                rasio = w/float(h)
+                if 0.9 < rasio < 1.1: benda = "Persegi"
+                else: benda = "Persegi Panjang"
+            #elif korner == 5: benda = "Segi lima"
+            #elif korner == 6: benda = "Segi enam"
+            #elif korner == 7: benda = "Segi tujuh"
+            #elif korner == 8: benda = "Segi delapan"
+            #elif korner <= 10000: benda = "Lingkaran"
+            else: benda = "Lainnya"
+
+            cv2.putText(imageFrame, benda+" BIRU", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.7, (255,0,0), 2) 
+    
+    cv2.imshow("green_mask",green_mask)
+    cv2.imshow("blue_mask", blue_mask)
+    cv2.imshow("red_mask", red_mask)
+    cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame)
+    if cv2.waitKey(10) & 0xFF == ord('q'): 
+        webcam.release() 
+        cv2.destroyAllWindows() 
+        break
